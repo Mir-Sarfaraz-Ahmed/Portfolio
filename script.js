7033e28fd1f48d54e4de4632d8200ecfaf10fc14
Mir Sarfaraz Ahmed <mirsarfarazahmed07@gmail.com>
2026-03-03 16:23:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ window.onload = function () {
 const form = document.getElementById("contact-form");
 const status = document.getElementById("form-status");
 
+if(form){
+
 form.addEventListener("submit", async function(e) {
 
 e.preventDefault();
 
 const data = new FormData(form);
+
+try {
 
 const response = await fetch(form.action, {
 method: "POST",
@@ -26,21 +30,13 @@ status.innerHTML = "Submission failed. Try again!";
 status.style.color = "red";
 }
 
+} catch(error) {
+status.innerHTML = "Network error. Please try again.";
+status.style.color = "red";
+}
+
 });
 
+}
+
 };
-
-function openModal(){
-document.getElementById("projectModal").style.display = "block";
-}
-
-function closeModal(){
-document.getElementById("projectModal").style.display = "none";
-}
-
-window.onclick = function(event) {
-const modal = document.getElementById("projectModal");
-if (event.target == modal) {
-modal.style.display = "none";
-}
-}
